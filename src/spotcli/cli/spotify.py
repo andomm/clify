09d78b946e0d83067ed3f22c4f7dd6df22e06c1c
Antton Anttila <antton.anttila01@gmail.com>
2025-03-02 12:52:16 +0200

    Authentication via the spotify authenticate command works now

--- src/spotcli/cli/spotify.py
+++ src/spotcli/cli/spotify.py
@@ -1,8 +1,11 @@
 import click
-from oauth2_cli_auth import OAuth2ClientInfo, get_access_token_with_browser_open
 
+from spotcli.clients.http_server import OAuthCallbackHttpServer
+from spotcli.clients.spotify_api import SpotifyClient
 from spotcli.common.config import config
 from spotcli.common.logger import log
+from spotcli.models import client_info
+from spotcli.models.client_info import OAuth2ClientInfo
 
 
 @click.group("spotify")
@@ -16,10 +19,11 @@ def get_current_song():
 
 @group.command("authenticate")
 def authenticate():
-    client_info = OAuth2ClientInfo(
-        "https://accounts.spotify.com/authorize",
-        "https://accounts.spotify.com/api/token",
+    oauth_client_info = OAuth2ClientInfo(
+        config.spotify_authorization_url,
+        config.spotify_token_url,
         config.spotify_client_id,
+        config.spotify_client_secret,
         [
             "user-read-private",
             "user-read-email",
@@ -27,8 +31,22 @@ def authenticate():
             "user-modify-playback-state",
         ],
     )
+    click.echo("Opening browser for authentication")
     try:
-        token = get_access_token_with_browser_open(client_info, 3000)
-        log.info(f"Token obtained: {token}")
+        callback_server = OAuthCallbackHttpServer(3000)
+
+        auth_url = client_info.get_auth_url(
+            oauth_client_info, callback_server.callback_url
+        )
+        client_info.open_browser(auth_url)
+        code = callback_server.wait_for_code()
+
+        with SpotifyClient(
+            config.spotify_client_id,
+            config.spotify_client_secret,
+            config.spotify_redirect_uri,
+        ) as client:
+            access_token, _ = client.get_tokens(code)
+            click.echo(f"Access token: {access_token}")
     except ValueError:
         log.error(f"Failed to obtain token")

f623940e6b265bcf9ca918471916fcfaf7ca93b0
Antton Anttila <antton.anttila01@gmail.com>
2025-03-08 20:34:57 +0200

    Pressing get current song button updates the label to the currently playing song

--- src/clify/cli/root.py
+++ src/clify/cli/root.py
@@ -1,12 +1,18 @@
 import click
 import dotenv
-from textual.app import App
-from textual.widgets import Button
+from textual.app import App, ComposeResult
+from textual.containers import Horizontal, VerticalScroll
+from textual.reactive import reactive
+from textual.widgets import Button, Label
 
 import clify.cli.spotify
+from clify.common import logic
 
 
-class SpotcliApp(App):
+class ClifyApp(App):
+
+    current_song = reactive("")
+
     def on_mount(self) -> None:
         self.title = "clify"
         self.screen.styles.background = "#1ED760"
@@ -14,11 +20,30 @@ class SpotcliApp(App):
         self.screen.styles.align_horizontal = "center"
         self.screen.styles.align_vertical = "middle"
 
-    def compose(self):
-        yield Button(
-            "Authenticate",
-            "default",
+    def compose(self) -> ComposeResult:
+        self.song_label = Label(f"Currently playing: {self.current_song}")
+        yield Horizontal(
+            VerticalScroll(
+                Button(
+                    "Authenticate",
+                    "default",
+                    name="authenticate",
+                ),
+                Button(
+                    "Current Song",
+                    "default",
+                    name="current-song",
+                ),
+                self.song_label,
+            )
         )
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.name == "authenticate":
+            logic.authenticate()
+        elif event.button.name == "current-song":
+            self.current_song = logic.get_current_song()
+            self.song_label.update(self.current_song)
 
 
 @click.group(
@@ -41,7 +66,7 @@ def group(
 
 @group.command("run")
 def cmd_run() -> None:
-    app = SpotcliApp()
+    app = ClifyApp()
     app.run()
 
 
